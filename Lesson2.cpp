8ef76889ab6a41c9bf0e2dd9b1c527b5bbe7bc72
mrmiklecrowin-wq <mr.miklecrowin@gmail.com>
2026-05-12 22:18:51 +0300

    Add files via upload

---
+++ Lesson2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+int number;
+int main() {
+	std::cin >> number;
+	std::cout << number << std::endl;
+	return 0;
+}
+
+/*
+#include <iostream>
+int number;
+int main() {
+	setlocale(LC_ALL,"Rus");
+	std::cout << "Введите число :";
+	std::cin >>  number;
+	std::cout << "Вы ввели число :";
+	std::cout << number << std::endl;
+	return 0;
+}
+*/
