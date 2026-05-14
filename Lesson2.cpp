ad79a0bb124465bb29989274581a2acdb16a71fd
mrmiklecrowin-wq <mr.miklecrowin@gmail.com>
2026-05-14 17:54:48 +0300

    Update Lesson2.cpp

--- Lesson2.cpp
+++ Lesson2.cpp
@@ -5,16 +5,3 @@ int main() {
 	std::cout << number << std::endl;
 	return 0;
 }
-
-/*
-#include <iostream>
-int number;
-int main() {
-	setlocale(LC_ALL,"Rus");
-	std::cout << "Введите число :";
-	std::cin >>  number;
-	std::cout << "Вы ввели число :";
-	std::cout << number << std::endl;
-	return 0;
-}
-*/
