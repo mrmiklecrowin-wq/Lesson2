d7dcde71f554fc049d0345deb6a8370c20572d54
mrmiklecrowin-wq <mr.miklecrowin@gmail.com>
2026-05-14 13:53:15 +0300

    Add files via upload

---
+++ Lesson 2_2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	std::cout << "Введите слово:"<< std::endl;
+	std::string words;
+	std::cin >> words;
+	std::cout << "Вы ввели:\n" << words << std::endl;
+
+	return 0;
+}
